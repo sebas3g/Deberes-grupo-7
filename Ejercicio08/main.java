03c57f15c3ea8992b3578f63583047aac2a9c911
Sebastian Navas <sebas19.fz@gmail.com>
2026-09-25 09:32:51 -0500

    Update main.java

--- Ejercicio08/main.java
+++ Ejercicio08/main.java
@@ -1,1 +1,12 @@
+public class depuradorr {
 
+    public static void main(String[] args) {
+
+        int numero = 1;
+
+        while (numero <= 5) {
+            System.out.println(numero);
+            numero++;
+        }
+    }
+}
